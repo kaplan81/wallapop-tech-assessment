bd6be90072adb442b8dd336aff7204c090e5e6e0
kaplan81 <gesteira.pro@gmail.com>
2022-12-04 21:50:18 +0100

    make app component standalone

--- projects/product-manager/src/app/app/containers/app/app.component.ts
+++ projects/product-manager/src/app/app/containers/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mng-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'product-manager';
