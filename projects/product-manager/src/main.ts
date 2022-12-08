be27a2a5e662fae3167b6285afd27d5adae07854
kaplan81 <gesteira.pro@gmail.com>
2022-12-08 10:20:21 +0100

    install angular material

--- projects/product-manager/src/main.ts
+++ projects/product-manager/src/main.ts
@@ -2,7 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app/containers/app/app.component';
 import { routes } from './app/app/routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule)],
 });
