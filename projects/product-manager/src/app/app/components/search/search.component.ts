b48c72db02712f17fb1356f4b900f7ba50c0dd5e
kaplan81 <gesteira.pro@gmail.com>
2022-12-11 22:23:46 +0100

    always go to page one on search submit

--- projects/product-manager/src/app/app/components/search/search.component.ts
+++ projects/product-manager/src/app/app/components/search/search.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ProductsStateService } from '../../services/products-state/products-state.service';
 
 @Component({
@@ -14,20 +14,13 @@ import { ProductsStateService } from '../../services/products-state/products-sta
 export class SearchComponent {
   #fb = inject(NonNullableFormBuilder);
   #productsStateService = inject(ProductsStateService);
-  #route = inject(ActivatedRoute);
   #router = inject(Router);
   searchForm = this.#fb.group({
     searchCriteria: [''],
   });
 
   onSubmit(): void {
-    let queryParams: Params = this.#route.snapshot.queryParams;
-    if (queryParams['page'] === undefined) {
-      queryParams = {
-        page: 1,
-      };
-    }
-    this.#router.navigate(['products'], { queryParams }).then(() => {
+    this.#router.navigate(['products'], { queryParams: { page: 1 } }).then(() => {
       this.searchForm.reset();
       this.#productsStateService.updateStateProp('loaded', false);
     });
