4f13ea01377ff9950f8e9de38411fbb416c440ad
kaplan81 <gesteira.pro@gmail.com>
2022-12-11 11:00:50 +0100

    create header

--- projects/product-manager/src/app/app/containers/app/app.component.ts
+++ projects/product-manager/src/app/app/containers/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  imports: [RouterModule, MatToolbarModule, MatButtonModule],
   selector: 'mng-root',
   templateUrl: './app.component.html',
   standalone: true,
