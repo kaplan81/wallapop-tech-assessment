7969bc77c5807f50ce99155e7c5bed0b844ad0c6
kaplan81 <gesteira.pro@gmail.com>
2022-12-11 13:51:13 +0100

    display real data

--- projects/product-manager/src/app/app/components/product/product.component.ts
+++ projects/product-manager/src/app/app/components/product/product.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ProductItem } from '../../models/product.model';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatCardModule],
+  imports: [NgIf, MatCardModule],
   standalone: true,
   styleUrls: ['./product.component.scss'],
   selector: 'mng-product',
   templateUrl: './product.component.html',
 })
-export class ProductComponent {}
+export class ProductComponent {
+  @Input() product: ProductItem | null = null;
+}
