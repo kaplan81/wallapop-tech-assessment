a49a2e519149b78eedd62cd47081782e54bf6023
kaplan81 <gesteira.pro@gmail.com>
2022-12-11 13:04:32 +0100

    create search component

---
+++ projects/product-manager/src/app/app/containers/search/search.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ReactiveFormsModule],
+  selector: 'mng-search',
+  standalone: true,
+  styleUrls: ['./search.component.scss'],
+  templateUrl: './search.component.html',
+})
+export class SearchComponent {
+  #fb = inject(NonNullableFormBuilder);
+  searchForm = this.#fb.group({
+    searchCriteria: [''],
+  });
+}
