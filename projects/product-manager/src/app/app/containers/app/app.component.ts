2ab00b4398c32388a11ce095b7c414c585b50ae7
kaplan81 <gesteira.pro@gmail.com>
2022-12-11 22:43:42 +0100

    disable favourites button

--- projects/product-manager/src/app/app/containers/app/app.component.ts
+++ projects/product-manager/src/app/app/containers/app/app.component.ts
@@ -1,21 +1,34 @@
-import { NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavigationEnd, Router, RouterEvent, RouterModule } from '@angular/router';
-import { filter } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
 import { SearchComponent } from '../../components/search/search.component';
+import { ProductItem } from '../../models/product.model';
+import { ProductsStateService } from '../../services/products-state/products-state.service';
 
 @Component({
-  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatInputModule, SearchComponent, NgIf],
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    SearchComponent,
+    NgIf,
+    AsyncPipe,
+  ],
   selector: 'mng-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  #favourites$: Observable<ProductItem[]>;
+  isFavouritesDisabled$: Observable<boolean>;
   isRootRoute = true;
+  #productsStateService = inject(ProductsStateService);
   #router = inject(Router);
 
   constructor() {
@@ -26,5 +39,9 @@ export class AppComponent {
        */
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event) => (this.isRootRoute = (event as RouterEvent).url === '/'));
+    this.#favourites$ = this.#productsStateService.getStateProp('favourites');
+    this.isFavouritesDisabled$ = this.#favourites$.pipe(
+      map((items: ProductItem[]) => items.length === 0),
+    );
   }
 }
