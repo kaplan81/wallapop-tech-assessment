fbac69ab74405283655e1981e52a6287c084b169
kaplan81 <gesteira.pro@gmail.com>
2022-12-13 11:13:56 +0100

    remove eslint comments

--- setup-jest.ts
+++ setup-jest.ts
@@ -3,10 +3,8 @@ import 'jest-preset-angular/setup-jest';
 const storageMock = () => {
   let storage: any = {};
   return {
-    // tslint:disable-next-line: no-dynamic-delete
     clear: () => (storage = {}),
     getItem: (key: string) => (key in storage ? storage[key] : null),
-    // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
     removeItem: (key: string) => delete storage[key],
     setItem: (key: string, value: any) => (storage[key] = value || ''),
   };
